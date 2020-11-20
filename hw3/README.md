2637cd3bb4882388fdb1e59043f12d02951d3255
JoosepAlbre <JoosepAlbre@users.noreply.github.com>
2020-11-21 01:54:01 +0200

    added routes

--- hw3/README.md
+++ hw3/README.md
@@ -1,4 +1,4 @@
-# hw3
+# vuetest
 
 ## Project setup
 ```
--- hw3/src/main.js
+++ hw3/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Login from './components/Login.vue'
+import Browse from './components/Browse.vue'
+import Index from './components/Index.vue'
 
-createApp(App).mount('#app')
+Vue.use(VueRouter)
+const routes = [
+    { path: '/', name: 'login', component: Login },
+    { path: '/browse/', name: 'browse', component: Browse },
+    { path: '/index/', name: 'index', component: Index }
+];
+
+const router = new VueRouter({ routes });
+
+
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app')
